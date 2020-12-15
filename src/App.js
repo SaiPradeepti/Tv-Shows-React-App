14b62923fb26042d5916df998ec37033752fdb72
SaiPradeepti <saipradeepti14@gmail.com>
2020-12-15 12:03:46 +0530

    Fetching Data from Contentful

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState,useEffect } from 'react'
+import client from './client'
 
 function App() {
+  const [data,setData] = useState([]);
+  const fetchData = async() => {
+      try {
+        const response = await client.getEntries();
+        const responseData = await response.items;
+        setData(responseData);
+      } 
+      catch (error) {
+          console.log(error)
+      }
+    }
+  
+  useEffect(() => {    
+    fetchData();    
+  }, [])
+  console.log(data);
   return (
     <>
       
