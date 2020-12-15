6c8078d221d07e1fd855c889ef3e12304e60b6e4
SaiPradeepti <saipradeepti14@gmail.com>
2020-12-15 12:04:42 +0530

    Adding client.js

---
+++ src/client.js
@@ -0,0 +1,8 @@
+import * as contentful from 'contentful'
+
+const client = contentful.createClient({
+    space: 'mf0dkrmqpe8y',
+    accessToken: 'XTd4PkSjVCcNMYSFcdvHt4j8km6EghbkaRppvDDiqaI'
+});
+
+export default client;
