e0376025a91b43c628e1d85f0a251d81be7aa1ba
SaiPradeepti <saipradeepti14@gmail.com>
2020-12-18 14:05:08 +0530

    Adding modal feature

--- src/shows.js
+++ src/shows.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import { CgMoreO } from "react-icons/cg";
+import { Modal } from 'react-responsive-modal';
 
 const Shows = (props) => {
+    // const [open, setOpen] = useState(false); 
+    const [open, setOpen] = useState([]); 
+  const onOpenModal = (id) => {
+      setOpen({id:id,value:true});
+  };
+  const onCloseModal = () => setOpen(false);
+  
     const newdata = props.data.map((item,index) => {
         const {title,image,year,description,genre} = item;
         return {title,image,year,description,genre,id:index+1}
     });
-    console.log(newdata);
+    
     return (
         newdata.map(item => {
-        const {title,image,year,description,genre,id} = item;
+        const {title,image,id} = item;
         return (
-            <div key={id} className="show">
+            <div key={id} className="show" data-key={id}>
                 <img src={image} alt="title" className="show-image"/>
-                <div className="show-icon">
-                    <CgMoreO />
+                <div className="show-filter"></div>
+                <div className="show-icon">                    
+                    <CgMoreO onClick={()=>onOpenModal(id)}/>
+                    {
+                        open && (
+                            <Modal open={open} onClose={onCloseModal} center
+                        classNames={{
+                        overlayAnimationIn: 'customEnterOverlayAnimation',
+                        overlayAnimationOut: 'customLeaveOverlayAnimation',
+                        modalAnimationIn: 'customEnterModalAnimation',
+                        modalAnimationOut: 'customLeaveModalAnimation',
+                        }}
+                        animationDuration={800}>
+                            <h2>{title}</h2>
+                    </Modal>
+                        )
+                    }
                 </div>
+
             </div>  
         );     
     })
     );
 }
-
+// ReactDOM.render(<Modal />, document.getElementById('root'));
 export default Shows;
