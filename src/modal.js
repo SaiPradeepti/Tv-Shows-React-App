81ddfb743c05a5ce78bb3186881c0bce9bf5b3ee
SaiPradeepti <saipradeepti14@gmail.com>
2020-12-21 00:23:17 +0530

    Adding Overlay and Modal Close Features

--- src/modal.js
+++ src/modal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Modal = (props) => {
-    // console.log(props);
     const {id,title,image,year,description,genre} = props.item;
     
     if(props.item.id === props.overlay.id){
@@ -9,14 +8,21 @@ const Modal = (props) => {
         <div>
             <div key={id} className="show-modalOverlay"></div>
             <div key={id} className="show-modal">
-                <div className="modal-ShowImage">
-                    <img src={image} alt={title}/>
-                </div>
-                <div className="modal-ShowDetails">
-                    <p>{title}</p>
-                    <p>{description}</p>
-                    <p>{genre}</p>
-                    <p>{year}</p>
+                <div className="modal">
+                    <div className="modal-ShowImage">
+                        <img src={image} alt={title}/>
+                    </div>
+                    <div className="modal-ShowDetails">
+                        <p style={{textAlign: 'center'}}>{title}</p>
+                        <p>Plot: {description}</p>
+                        <p>Genre: {genre}</p>
+                        <p>Year: {year}</p>
+                        <button className="modal-close" onClick={() => {
+                            console.log('close modal');
+                            document.querySelector('.show-modalOverlay').classList.add('close');
+                            document.querySelector('.show-modal').classList.add('close');
+                        }}>close</button>
+                    </div>
                 </div>
             </div>
         </div>
